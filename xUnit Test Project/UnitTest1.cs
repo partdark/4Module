7cd45f23eb03f71c5e4990d8bc97f3ddcd815cfe
User <user@example.com>
2025-10-29 17:05:25 +0500

    TASK-379: Выполнен Parameterized Tests ([Theory]), тест выполняется с разными данными

--- xUnit Test Project/UnitTest1.cs
+++ xUnit Test Project/UnitTest1.cs
@@ -5,16 +5,26 @@ namespace xUnit_Test_Project
         [Fact]
         public void Test1()
         {
-            var resualt =  Math.Add(1, 2);
+            var resualt = Math.Add(1, 2);
             var exepted = 1 + 2;
 
             Assert.Equal(exepted, resualt);
         }
 
-
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(5, 5, 10)]
+        public void TestAdd(double a, double b, double exepted)
+        {
+            var result = Math.Add(a, b);
+            Assert.Equal(exepted, result);
+        }
     }
+
     public static class Math
     {
         public static double Add(double a, double b) => a + b;
+
+
     }
 }
