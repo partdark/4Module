defa097b2ea57962d3cb075cc1391add63f59239
User <user@example.com>
2025-10-29 17:28:21 +0500

    TASK-379: Добавлено Moq тестирование, которое не касается базы данных

--- xUnit Test Project/UnitTest1.cs
+++ xUnit Test Project/UnitTest1.cs
@@ -1,3 +1,11 @@
+using Application;
+using Applications.Services;
+using Domain.Entitties;
+using Domain.Interfaces;
+using Moq;
+using Repository;
+using System.Threading.Tasks;
+
 namespace xUnit_Test_Project
 {
     public class UnitTest1
@@ -6,20 +14,60 @@ namespace xUnit_Test_Project
         public void Test1()
         {
             var resualt = Math.Add(1, 2);
-            var exepted = 1 + 2;
+            var expected = 1 + 2;
 
-            Assert.Equal(exepted, resualt);
+            Assert.Equal(expected, resualt);
         }
 
         [Theory]
         [InlineData(1, 2, 3)]
         [InlineData(5, 5, 10)]
-        public void TestAdd(double a, double b, double exepted)
+        public void TestAdd(double a, double b, double expected)
         {
             var result = Math.Add(a, b);
-            Assert.Equal(exepted, result);
+            Assert.Equal(expected, result);
         }
     }
+
+    
+    public  class BookReposotoryTest
+    {
+        private readonly Mock<IBookRepository> _bookMock;
+        private readonly Mock<IAuthorRepository> _authorMock;
+        private readonly IBookService _bookService;
+
+
+        public BookReposotoryTest()
+        {
+            _bookMock = new Mock<IBookRepository>();
+            _authorMock = new Mock<IAuthorRepository>();
+            _bookService = new BookService(_bookMock.Object, _authorMock.Object);
+
+        }
+
+
+        [Fact]
+        public async Task GetByIdtest()
+        {
+            var bookId = Guid.NewGuid();
+            var expectedBook = new Book
+            {
+                Id = bookId,
+                Authors = new List<Author>(),
+                Title = "testTitle",
+                Year = 1985
+            };
+            _bookMock.Setup(b => b.GetByIdAsync(bookId)).ReturnsAsync(expectedBook);
+
+            var result = await _bookService.GetByIdAsync(bookId);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedBook.Id, result.Id);
+
+        }
+
+    }
+
 
     public static class Math
     {
