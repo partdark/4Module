0b2e3ae06230a5f685f02cad58db8be713817474
User <user@example.com>
2025-11-05 14:45:59 +0500

    TASK-383: Была отправлена команда в шину и получен код 202 при тестировании

---
+++ 4Module/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿
+
+using Application.DTO;
+using Application.Interfaces;
+using Domain.Interfaces;
+using MassTransit;
+using MassTransit.Internals;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver.Core.Operations;
+using OrderContracts;
+using System.Security.Cryptography.Xml;
+
+namespace _4Module.Controllers
+{
+    /// <summary>
+    /// Controller for orders
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+
+
+    public class OrderController : ControllerBase
+    {
+
+        private readonly IBus _bus;
+
+
+        public OrderController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Publish([FromBody] SubmitOrderCommand submitOrderCommand)
+        {
+            await _bus.Publish(submitOrderCommand);
+            return Accepted();
+
+        }
+
+
+    }
+}
